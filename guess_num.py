aa509a0dee37a47538cf41a7aa6ac81ce0b56049
sunnie1239 <sunnie1239@gmail.com>
2020-03-27 14:38:49 -0700

    User insert range

--- guess_num.py
+++ guess_num.py
@@ -1,7 +1,15 @@
+# version 1 猜1~100的一個數
+# version 2 加上猜測次數
+# version 3 改由使用者設置隨機數起迄
 import random
 
-r = random.randint(1, 100)  #抓出一個數
+start = input('請輸入起始值:')
+end   = input('請輸入終止值:')
+start = int(start)
+end   = int(end)
+r     = random.randint(start, end)  #產生隨機數
 count = 0
+
 while True :
 	num = input('猜猜數字是多少:')
 	num = int(num)

3c1459c72d1259a7cc01962d56214adeeddc9d67
sunnie1239 <sunnie1239@gmail.com>
2020-03-27 14:28:11 -0700

    Add count

--- guess_num.py
+++ guess_num.py
@@ -1,9 +1,11 @@
 import random
 
 r = random.randint(1, 100)  #抓出一個數
+count = 0
 while True :
 	num = input('猜猜數字是多少:')
 	num = int(num)
+	count += 1 # count = count + 1
 	if num == r :
 		print('Bingo!!!答案就是', r)
 		break
@@ -11,3 +13,5 @@ while True :
 		print('答案比', num, '小')
 	elif num < r :
 		print('答案比', num, '大')
+
+print('總共猜了', count, '次')
